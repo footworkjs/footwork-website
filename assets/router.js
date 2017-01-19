0830d88486840d9cebca48e63542cf9075fc530e
Jonathan Newman <jonathan@overfoc.us>
2017-01-19 07:27:22 -0600

    fixed routing issues

--- assets/router.js
+++ assets/router.js
@@ -1,6 +1,6 @@
 define(['footwork'], function (fw) {
   fw.components.registerLocation('main-page', { template: '/main.html' });
-  fw.components.registerLocation('about-page', { template: '/about' });
+  fw.components.registerLocation('about-page', { template: '/about/index.html' });
 
   return function Router () {
     var self = fw.router.boot(this, {
