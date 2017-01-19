1ee7084caa98234508430a83b2ebe4308eb6523e
Jonathan Newman <jonathan@overfoc.us>
2017-01-19 07:04:37 -0600

    initial working footwork based page routing

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-    "text": "/assets/bower_components/requirejs-text/text",
+    "text": "/assets/bower_components/text/text",
     "footwork": "/assets/bower_components/footwork/dist/footwork",
     "es6-promise": "/assets/bower_components/es6-promise/es6-promise",
     "fetch": "/assets/bower_components/fetch/fetch",
--- assets/router.js
+++ assets/router.js
@@ -1,4 +1,7 @@
 define(['footwork'], function (fw) {
+  fw.components.registerLocation('main-page', { template: '/main.html' });
+  fw.components.registerLocation('about-page', { template: '/about' });
+
   return function Router () {
     var self = fw.router.boot(this, {
       namespace: 'Router',
@@ -6,7 +9,13 @@ define(['footwork'], function (fw) {
         {
           route: '/',
           controller: function () {
-            console.info('default route');
+            this.outlet('main', 'main-page');
+          }
+        },
+        {
+          route: '/about',
+          controller: function () {
+            this.outlet('main', 'about-page');
           }
         }
       ]
