e7e74cdfcfbf6f3ef5f214291dd1559c0ad2f116
Jonathan Newman <jonathan@overfoc.us>
2017-01-21 01:11:32 -0600

    added example display to home page

--- assets/router.js
+++ assets/router.js
@@ -2,6 +2,12 @@ define(['footwork', 'jquery', 'highlightjs'], function (fw, $) {
   fw.components.registerLocation('main-page', { template: '/main.html' });
   fw.components.registerLocation('about-page', { template: '/about/index.html' });
 
+  function handleTextResponse (response) {
+    if (response.ok) {
+      return response.text();
+    }
+  }
+
   return function Router () {
     var self = fw.router.boot(this, {
       namespace: 'Router',
@@ -9,11 +15,31 @@ define(['footwork', 'jquery', 'highlightjs'], function (fw, $) {
         {
           route: '/',
           controller: function () {
-            this.outlet('main', { display: 'main-page', onComplete: function (container) {
-              $(container).find('pre code').each(function(i, block) {
-                hljs.highlightBlock(block);
-              });
-            }});
+            this.outlet('main', {
+              display: 'main-page',
+              onComplete: function (container) {
+                var $examples = $(container).find('.example.container');
+
+                var loaded = 0;
+                function triggerAfterLoad () {
+                  loaded++;
+                  if(loaded > 1) {
+                    $(container).find('pre code').each(function(i, block) {
+                      hljs.highlightBlock(block);
+                    });
+                  }
+                }
+
+                fetch('/assets/example/example.js').then(handleTextResponse).then(function (data) {
+                  $examples.find('.javascript').text(data);
+                  triggerAfterLoad();
+                });
+                fetch('/assets/example/example.html').then(handleTextResponse).then(function (data) {
+                  $examples.find('.html').text(data);
+                  triggerAfterLoad();
+                });
+              }
+            });
           }
         },
         {
